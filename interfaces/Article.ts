82df84433bf6236d9b07e2e0ac702c7c04679b15
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2023-04-23 01:43:41 +0530

    small fix in API related to type error

--- interfaces/Article.ts
+++ interfaces/Article.ts
@@ -3,14 +3,14 @@ export interface Article {
   title: string;
   link: string;
   description: string | null;
-  pub_date: string | null;
+  pub_date: Date | null;
   image_url: string | null;
   category: string | null;
   creator: string | null;
   source: string | null;
   country: string | null;
   keywords: string | null;
-  addedToTableAt: string | null;
+  addedToTableAt: Date | null;
   summarized_text: string | null;
   predicted_sentiment: string | null;
   predicted_emotion: string | null;
