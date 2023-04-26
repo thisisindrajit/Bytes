44aa36b0939432a31245c862acdf6b2e25e2aa7e
Indrajit Kutuva <ikutuva@microsoft.com>
2023-04-26 19:17:45 +0530

    removed transition for modal to slightly increase perf

--- components/common/InfoModal.tsx
+++ components/common/InfoModal.tsx
@@ -19,19 +19,19 @@ const InfoModal: FC<InfoModalProps> = ({
       <Dialog as="div" className="relative z-10" onClose={onClose}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-200"
           enterFrom="opacity-0"
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-75" />
+        <div className="fixed inset-0 bg-black bg-opacity-75" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
+            {/* <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
               enterFrom="opacity-0 scale-95"
@@ -39,29 +39,26 @@ const InfoModal: FC<InfoModalProps> = ({
               leave="ease-in duration-200"
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full lg:max-w-[calc(80vw)] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                {/* Title and close button */}
-                <div className="flex items-center justify-between">
-                  {/* Title */}
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-black capitalize"
-                  >
-                    {title}
-                  </Dialog.Title>
-                  {/* Close button */}
-                  <div
-                    onClick={onClose}
-                    className="text-red-500 cursor-pointer"
-                  >
-                    Close
-                  </div>
+            > */}
+            <Dialog.Panel className="w-full lg:max-w-[calc(80vw)] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              {/* Title and close button */}
+              <div className="flex items-center justify-between">
+                {/* Title */}
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-black capitalize"
+                >
+                  {title}
+                </Dialog.Title>
+                {/* Close button */}
+                <div onClick={onClose} className="text-red-500 cursor-pointer">
+                  Close
                 </div>
-                {/* Modal content goes here */}
-                <div className="my-6">{children}</div>
-              </Dialog.Panel>
-            </Transition.Child>
+              </div>
+              {/* Modal content goes here */}
+              <div className="my-6">{children}</div>
+            </Dialog.Panel>
+            {/* </Transition.Child> */}
           </div>
         </div>
       </Dialog>
