58fe40c3eaa19655ea30d9459cec0768a9c72d1b
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2023-05-02 21:21:12 +0530

    fixed bug in infinite scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,7 @@ const Home = () => {
                 >
                   <ArticleHolder
                     id={article.id}
-                    className="min-h-[100dvh] snap-center p-4"
+                    className="min-h-[100dvh] snap-always snap-center p-4"
                     hasPrevious={index === 0 ? false : true}
                     hasNext={index === articlesData.length - 1 ? false : true}
                     prevId={articlesData[index - 1]?.id}
